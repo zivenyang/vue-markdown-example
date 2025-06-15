be09b44abd22b9d26137e064a4eaae1808050d97
杨子文 <yangziwen1994@gmail.com>
2025-06-15 16:04:32 +0800

    fix: 优化部署脚本

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_URL || '/',
   plugins: [
     vue(),
     vueDevTools(),
