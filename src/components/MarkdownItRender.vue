fd53065acb816ffc643a939044b8d23a294f0352
杨子文 <yangziwen1994@gmail.com>
2025-06-15 10:43:11 +0800

    feat: 添加markdown-it

---
+++ src/components/MarkdownItRender.vue
@@ -0,0 +1,15 @@
+<template>
+    <div v-html="html"></div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+import MarkdownIt from 'markdown-it'
+
+const props = defineProps<{
+    content: string
+}>()
+
+const md = new MarkdownIt()
+
+const html = ref(md.render(props.content))
+</script>
